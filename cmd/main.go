0359b065bedec1119164d3e2e0a6432977b64d53
molokovskikh <mlh@rdtc.ru>
2018-08-10 14:26:38 +0700

    Update Refs

--- cmd/main.go
+++ cmd/main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"github.com/docker/machine/libmachine/drivers/plugin"
-	"github.com/pecigonzalo/docker-machine-vmwareworkstation"
+	"github.com/molokovskikh/docker-machine-vmwareworkstation"
 )
 
 func main() {
